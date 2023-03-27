ae4ee2ffeab00e7702c3c8d6c006f0bed409d293
Mougily <92stephaniejones@gmail.com>
2023-03-27 13:45:51 +0100

    some added styling and logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Home from "./routes/home/home.component";
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+
+
+const Shop = () => {
+  return (<h1>this is the Shop</h1>)
+}
+
+
 function App() {
-  return (<Routes>
-    <Route path='/'element={ <Home />}/>
-    </Routes>);
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route path='/shop' element={<Shop/>}></Route>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
